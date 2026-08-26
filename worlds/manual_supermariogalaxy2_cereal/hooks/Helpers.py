6b54de4fa36b4b1dd1be176808952553da960dec
carbon-cosmic <corecarboncosmos@gmail.com>
2026-08-26 08:52:07 -0700

    A bunch of options stuff - no hooks

--- worlds/manual_supermariogalaxy2_cereal/hooks/Helpers.py
+++ worlds/manual_supermariogalaxy2_cereal/hooks/Helpers.py
@@ -1,7 +1,6 @@
 from typing import Optional, Any
 from BaseClasses import MultiWorld
 
-
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the category, False to disable it, or None to use the default behavior
 def before_is_category_enabled(multiworld: MultiWorld, player: int, category_name: str) -> Optional[bool]:
